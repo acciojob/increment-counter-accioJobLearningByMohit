290d1460dcaa2bb64af5b106fe3748781195dd6d
accioJobLearningByMohit <mohitzod56062@gmail.com>
2026-04-06 20:17:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+let counterEl = document.getElementById("counter");
+let btn = document.getElementById("incrementBtn");
+btn.addEventListener("click", function() {
+    let currentValue = Number(counterEl.innerText);
+
+    // Show alert BEFORE increment
+    alert(currentValue);
+
+    // Increment value
+    currentValue++;
+
+    // Update DOM
+    counterEl.innerText = currentValue;
+});
